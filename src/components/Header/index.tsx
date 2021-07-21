fcdbabbb60d0f70dda41b35bea6d4c394e132d0d
Maykon Morais <maykon.estudos@gmail.com>
2021-07-21 13:11:59 -0300

    feat: add landing Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,24 @@
+import { Instagram, Twitter, Youtube } from 'react-feather'
+
+export default function Header() {
+	return (
+		<head
+			className='container d-flex align-items-center justify-content-between'
+			style={{ height: '70px' }}
+		>
+			<img className='img-fluid h-75' src='logo.svg' alt='Git Scan Logo' />
+
+			<div className='d-flex'>
+				<div className='rounded-circle align-items-center'>
+					<Instagram className='text-white' size={26} />
+				</div>
+				<div className='rounded-circle align-items-center ms-3'>
+					<Twitter className='text-white' size={26} />
+				</div>
+				<div className='rounded-circle align-items-center ms-3'>
+					<Youtube className='text-white' size={26} />
+				</div>
+			</div>
+		</head>
+	)
+}
