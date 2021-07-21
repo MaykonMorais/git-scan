518fec4520be0c665008ad2bef960de06252ea32
Maykon Morais <maykon.estudos@gmail.com>
2021-07-21 13:12:51 -0300

    styles: cadd sass styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 // ** Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.css'
+import '@src/styles/scss/global.scss'
 
 // ** Components
 import Head from '@components/Head'
+
+import DefaultLayout from '@src/components/Layout/Default'
 
 function MyApp({ Component, pageProps }: AppProps) {
 	const keywords = ['git', 'github']
@@ -17,12 +18,13 @@ function MyApp({ Component, pageProps }: AppProps) {
 				<link rel='preconnect' href='https://fonts.googleapis.com' />
 				<link rel='preconnect' href='https://fonts.gstatic.com' />
 				<link
-					href='https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800&display=swap'
+					href='https://fonts.googleapis.com/css2?family=Inter:wght@100;400;500;600;700;800&family=Montserrat:wght@300&display=swap'
 					rel='stylesheet'
 				/>
 			</Head>
-
-			<Component {...pageProps} />
+			<DefaultLayout>
+				<Component {...pageProps} />
+			</DefaultLayout>
 		</>
 	)
 }
