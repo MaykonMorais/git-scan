82efeb3744eaec3fe964cc9bd7db0968397cf6f2
Maykon Morais <maykon.estudos@gmail.com>
2021-07-22 12:15:09 -0300

    feat: add per_page param on search repositories

--- src/services/api.ts
+++ src/services/api.ts
@@ -12,7 +12,7 @@ function searchRepositories(searchText: string, language: string) {
 	const query = language ? `${searchText}+language:${language}` : searchText
 
 	return axios.get(
-		`/search/repositories?q=${query}&sort=stars&order=desc`,
+		`/search/repositories?q=${query}&sort=stars&order=desc&per_page=5`,
 		axiosConfig
 	)
 }
