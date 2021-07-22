f5ecdca2eb2fd4ea9d513d90bf27c8f8e1e6a985
Maykon Morais <maykon.estudos@gmail.com>
2021-07-22 02:34:22 -0300

    feat: add new user page and jwt properties

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -2,12 +2,16 @@ import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
 export default NextAuth({
-	// Configure one or more authentication providers
 	providers: [
 		Providers.GitHub({
 			clientId: process.env.GITHUB_ID,
 			clientSecret: process.env.GITHUB_SECRET,
 		}),
-		// ...add more providers here
 	],
+	pages: {
+		newUser: '/search',
+	},
+	jwt: {
+		signingKey: process.env.JWT_SIGNING_PRIVATE_KEY,
+	},
 })
